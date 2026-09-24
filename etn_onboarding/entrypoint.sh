b355c76f151bc55e52b9325a5c4595a6e48d576a
mumu <azal88@yahoo.com>
2026-09-23 23:36:35 -0500

    dockerfile change

--- etn_onboarding/entrypoint.sh
+++ etn_onboarding/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+echo "Ensuring enum types exist..."
+python -c "
+import os, psycopg2
+conn = psycopg2.connect(os.environ['DATABASE_URL'])
+conn.autocommit = True
+cur = conn.cursor()
+for stmt in [
+    \"DO \$\$ BEGIN CREATE TYPE environment_enum AS ENUM ('dev','stage','prod'); EXCEPTION WHEN duplicate_object THEN NULL; END \$\$;\",
+    \"DO \$\$ BEGIN CREATE TYPE request_status_enum AS ENUM ('intake_pending','intake_validated','engagement','solutioning','storage_pending','storage_confirmed','delivery_destination','delivery_pack','delivery_route','delivery_collection','delivery_routing','delivery_storage','delivery_complete','delivery_failed','validation','reverify','complete','cancelled'); EXCEPTION WHEN duplicate_object THEN NULL; END \$\$;\",
+    \"DO \$\$ BEGIN CREATE TYPE job_type_enum AS ENUM ('cribl_edge','etn_portal','harness_blob'); EXCEPTION WHEN duplicate_object THEN NULL; END \$\$;\",
+    \"DO \$\$ BEGIN CREATE TYPE job_status_enum AS ENUM ('pending','running','success','failed'); EXCEPTION WHEN duplicate_object THEN NULL; END \$\$;\",
+]:
+    cur.execute(stmt)
+cur.close()
+conn.close()
+print('Enum types ready.')
+"
+
 echo "Running database migrations..."
 flask --app wsgi:app db upgrade
 
